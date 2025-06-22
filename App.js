f6875812e511c45e3d9e1930a97c6f179ce23a70
ChatViewsource <chatviewsupporyou@gmail.com>
2025-06-22 20:45:42 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import NewPostScreen from './screens/NewPostScreen';
+import ChatScreen from './screens/ChatScreen';
+import { I18nManager } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NewPost" component={NewPostScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
